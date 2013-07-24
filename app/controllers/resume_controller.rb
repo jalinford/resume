984e8934ff4a1e127604b2d99e719cd66a1260c1
oleger814 <jandrewl814@yahoo.com>
2013-07-23 18:25:53 -0700

    Sub-nav basic added

--- app/controllers/resume_controller.rb
+++ app/controllers/resume_controller.rb
@@ -1,10 +1,12 @@
 class ResumeController < ApplicationController
   def index
   	@tags = Tag.where(featured: true)
+  	@all_tags = Tag.all
   end
 
   def tag
   	@tags = Tag.where(featured: true)
-  	@tags_all = Tag.find_all_by_title(params[:title].split('/'))
+  	@all_tags = Tag.all
+  	@tags_displayed = Tag.find_all_by_title(params[:title].split('/'))
   end
 end
--- app/helpers/resume_helper.rb
+++ app/helpers/resume_helper.rb
@@ -1,2 +1,10 @@
 module ResumeHelper
+	def all_titles
+		all_titles_array = []
+		@all_tags.each do |tag|
+			all_titles_array.push(tag.title)
+		end
+		all_titles = all_titles_array.sort.join('/')
+		return all_titles
+	end
 end
